dd84cc5e8d9f16f44f7452a0e0dc7db06dcf9918
renascm_ <renatochagas.m@gmail.com>
2024-12-03 00:31:43 -0300

    chore community

--- src/components/community/bgBox.vue
+++ src/components/community/bgBox.vue
@@ -3,10 +3,33 @@
 </script>
 
 <template>
-    <main class="w-full h-full flex  bg-[#121212] ">
-        <slot>
+    <div class="flex flex-col w-[100%] h-screen p-2 gap-2">
+        <div class="w-full ">
+            <h2 class="text-xl font-extralight">Comunidades Que Decolam</h2>
+        </div>
+        <div class="w-[100%] max-w-[95%] h-[30%] gap-2">
+            <div class="flex flex-col justify-center w-[30%] h-full relative">
+                <div class="flex flex-col justify-center w-full h-[100%]  ">
+                    
+                    <div>
+                    <img src="../../images/26427-3840x2160-desktop-4k-black-hole-wallpaper-image.jpg"
+                        class="relative w-[100%] h-[50%]  bg-blue-700 rounded-t-lg">
+                    </div>
 
-        </slot>
-    </main>
+                    <div class="flex ml-4 absolute z-[1] max-w-[95%] h-14">
+                        <div>
+                            <img class="w-14 h-14 rounded-full object-contain bg-red-400">
+                        </div>
+                        <div class="flex last:items-end">
+                            <h1 class="w-full align-bottom">Astronomos independentes</h1>
+                        </div>
+                    </div>
+
+                    <div class="relative w-[100%] h-[50%] bg-gray-800 rounded-b-lg">
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
-
