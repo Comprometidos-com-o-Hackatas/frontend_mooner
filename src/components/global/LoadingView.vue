c3fe58d9a9c9f58dead174d5e98cc9725bfa537e
renascm <renatochagas.m@gmail.com>
2024-12-10 14:38:57 -0300

    feat: created loading component #75

---
+++ src/components/global/LoadingView.vue
@@ -0,0 +1,10 @@
+<script setup>
+import MoonerLoading from "@/assets/video/MoonerLoading.gif"
+</script>
+<template>
+    <div class="w-screen h-screen flex justify-center items-center">
+        <img class="absolute z-[100]" :src="MoonerLoading" alt="" >
+    </div>
+</template>
+
+<style></style>
